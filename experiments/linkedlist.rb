d960f19122f516abe455bc47adbfe75e4652ad57
Amber Nash <nash.beram@gmail.com>
2017-05-14 13:34:03 -0700

    Add experiments folder.

---
+++ experiments/linkedlist.rb
@@ -0,0 +1,30 @@
+class Node
+
+  attr_accessor :value, :next_node
+
+  def initialize(value, next_node)
+    @value = value
+    @next_node = next_node
+  end
+
+end
+
+class LinkedList
+
+  def initialize(value)
+    @head = Node.new(value)
+  end
+
+  def add(value)
+    current = @head
+    while !current.next_node.nil?
+      current = current.next_node
+    end
+    current.next_node = Node.new(value)
+  end
+
+ end
+
+ ll = LinkedList.new
+ ll.add(10)
+ ll.add(20)
